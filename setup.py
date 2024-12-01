6c4e9b7e84ed96e42c2217a02d3ab5444e5ceee2
NeurosystemsCo <maria@neurosystems.co>
2024-12-01 22:01:06 +0800

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/NeurosystemsCo/manychrome/setup.py
+
 from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
 
 setup(
     name="manychrome",
     version="0.0.1",
     author="Maria Wenner",
     author_email="mail@neurosystems.co",
-    description="A simple package make life easier when working with text from the CLI.",
+    description="A simple package to colour and style your text printed on the CLI.",
+    long_description=readme,
     packages=find_packages(),
     classifiers=[
     "Programming Language :: Python :: 3",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent"
+    "Operating System :: MacOS"
     ],
     python_requires='>=3.8',
     homepage = "https://github.com/NeurosystemsCo/manychrome",
